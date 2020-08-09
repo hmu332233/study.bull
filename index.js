3805021d12f9015caa4160d0eb1680fa2db03287
minung.han <hmu332233@gmail.com>
2020-08-10 01:28:35 +0900

    job의 처리 속도가 다를때 queue 테스트

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const Queue = require('bull');
 
 
-const testQueue = new Queue('testQueue', {
+const testQueue = new Queue('testQueue2', {
   redis: {
     host: '',
-    port: ,
+    port: 0,
   }
 });
 
 const options = {
-  delay: 60000, // 1 min in ms
+  // delay: 60000, // 1 min in ms
   attempts: 2
 };
 
-// 2. Adding a Job to the Queue
-testQueue.add({ data: 'hello!' }, options);
-
-
 // 3. Consumer
-testQueue.process(async job => {
-  console.log(job)
-  return new Promise((resolve, reject) => {
-    resolve();
-  });
+testQueue.process(async (job, done) => {
+  console.log(job.id, job.attemptsMade)
+  // job.data contains the custom data passed when the job was created
+  // job.id contains id of this job.
+
+
+  setTimeout(() => {
+    done(null, job.data);
+  }, job.data);
+  // or give a error if error
+  
+
 });
+
+testQueue.on('completed', function(job, result){
+  console.log(result);
+  // Job completed with output result!
+})
+
+
+testQueue.add(100, options);
+testQueue.add(1000, options);
+testQueue.add(100, options);
+testQueue.add(1000, options);
